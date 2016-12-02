f756b9c080cfbf31ed18999192243d69c3398e3f
Yif Swery <yiftachswr@gmail.com>
2016-12-03 00:32:46 +1300

    Adjust key names on the tunnel creation

--- app/Services/TunnelService.php
+++ app/Services/TunnelService.php
@@ -9,22 +9,22 @@ class TunnelService
 {
 
     // Allocate a tunnel address and prefix
-    public function createTunnelCombo($cidrSize = 48, $userId, $remoteAddress)
+    public function createTunnelCombo($userId, $remoteAddress, $cidrSize = 48)
     {
         // Get available address
         $tunnel = Tunnel::whereNull('user_id')->first();
 
         // Set the remote address and user ID
-        $tunnel->remote_address = $remoteAddress;
-        $tunnel->user_id        = $userId;
+        $tunnel->remote_v4_address = $remoteAddress;
+        $tunnel->user_id           = $userId;
         $tunnel->save();
 
         $tunnelPrefix = $this->allocateTunnelPrefix($cidrSize, $tunnel);
         // TO DO: Provision the tunnel address and interface on local tunnel server
 
         return [
-            'tunnel' => $tunnel,
-            'tunnel_prefix'  => $tunnelPrefix,
+            'tunnel'        => $tunnel,
+            'tunnel_prefix' => $tunnelPrefix,
         ];
     }
 
@@ -34,8 +34,8 @@ class TunnelService
         // Get available prefix
         $tunnelPrefix = TunnelPrefix::whereNull('user_id')->where('cidr', $cidrSize)->first();
 
-        $tunnelPrefix->user_id           = $tunnel->user_id;
-        $tunnelPrefix->tunnel_address_id = $tunnel->id;
+        $tunnelPrefix->user_id   = $tunnel->user_id;
+        $tunnelPrefix->tunnel_id = $tunnel->id;
         $tunnelPrefix->save();
 
         // TO DO: Route the prefix through the existing tunnel address
