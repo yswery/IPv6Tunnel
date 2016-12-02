ca1d6f2528be75a070de2dee2e22468bbdea14d7
Yif Swery <yiftachswr@gmail.com>
2016-12-03 09:43:00 +1300

    Add functionality to remove a tunnel and prefixes

--- app/Services/TunnelService.php
+++ app/Services/TunnelService.php
@@ -35,7 +35,7 @@ class TunnelService
     }
 
     // Allocate prefix and route through to an existant tunnel
-    public function allocateTunnelPrefix($cidrSize = 48, $tunnel)
+    public function allocateTunnelPrefix($cidrSize = 48, Tunnel $tunnel)
     {
         // Get available prefix
         $tunnelPrefix = TunnelPrefix::whereNull('user_id')->where('cidr', $cidrSize)->first();
@@ -48,6 +48,44 @@ class TunnelService
         \SSH::run([
             'ip route add ' . $tunnelPrefix->address . '/' . $tunnelPrefix->cidr . ' dev ' . $tunnel->local_interface,
         ]);
+
+        return $tunnelPrefix;
+    }
+
+    // Remove the tunnel prefix from a user and tunnel
+    public function removeTunnel(Tunnel $tunnel)
+    {
+        // Remove all tunnel prefixes associated with the tunnel
+        foreach ($tunnel->prefixes as $tunnelPrefix) {
+            $this->removeTunnelPrefix($tunnelPrefix);
+        }
+
+        // Provision the tunnel address and interface on local tunnel server
+        \SSH::run([
+            'ip addr del ' . $tunnel->local_tunnel_address . '/64 dev ' . $tunnel->local_interface,
+            'ip link set ' . $tunnel->local_interface . ' down',
+            'ip tunnel del ' . $tunnel->local_interface,
+        ]);
+
+        $tunnel->remote_v4_address = null;
+        $tunnel->user_id           = null;
+        $tunnel->save();
+
+        return $tunnel;
+    }
+
+    // Remove the tunnel prefix from a user and tunnel
+    public function removeTunnelPrefix(TunnelPrefix $tunnelPrefix)
+    {
+        // Remove the static route from the tunnel server node
+        \SSH::run([
+            'ip route del ' . $tunnelPrefix->address . '/' . $tunnelPrefix->cidr . ' dev ' . $tunnelPrefix->tunnel->local_interface,
+        ]);
+
+        // Reset the prefix to null and add back to pool
+        $tunnelPrefix->user_id = null;
+        $tunnelPrefix->tunnel_id = null;
+        $tunnelPrefix->save();
 
         return $tunnelPrefix;
     }
