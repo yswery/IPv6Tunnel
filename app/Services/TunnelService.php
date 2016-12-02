709fc84af80a6b9581529446b24472c14243bdce
Yif Swery <yiftachswr@gmail.com>
2016-12-02 23:52:31 +1300

    Create initial allocation of a tunnel addressing and prefix

--- app/Services/TunnelService.php
+++ app/Services/TunnelService.php
@@ -8,11 +8,9 @@ use App\Models\TunnelPrefix;
 class TunnelService
 {
 
-    public function allocateTunnelPrefix($cidrSize = 48, $userId, $remoteAddress)
+    // Allocate a tunnel address and prefix
+    public function createTunnelCombo($cidrSize = 48, $userId, $remoteAddress)
     {
-        // Get available prefix
-        $tunnelPrefix = TunnelPrefix::whereNull('user_id')->where('cidr', $cidrSize)->first();
-
         // Get available address
         $tunnelAddress = TunnelAddress::whereNull('user_id')->first();
 
@@ -21,13 +19,28 @@ class TunnelService
         $tunnelAddress->user_id        = $userId;
         $tunnelAddress->save();
 
-        $tunnelPrefix->user_id           = $userId;
+        $tunnelPrefix = $this->allocateTunnelPrefix($cidrSize, $tunnelAddress);
+        // TO DO: Provision the tunnel address and interface on local tunnel server
+
+        return [
+            'tunnel_address' => $tunnelAddress,
+            'tunnel_prefix'  => $tunnelPrefix,
+        ];
+    }
+
+    // Allocate prefix and route through to an existant tunnel
+    public function allocateTunnelPrefix($cidrSize = 48, $tunnelAddress)
+    {
+        // Get available prefix
+        $tunnelPrefix = TunnelPrefix::whereNull('user_id')->where('cidr', $cidrSize)->first();
+
+        $tunnelPrefix->user_id           = $tunnelAddress->user_id;
         $tunnelPrefix->tunnel_address_id = $tunnelAddress->id;
         $tunnelPrefix->save();
 
-        return [
-            'tunnel_prefix'  => $tunnelPrefix,
-            'tunnel_address' => $tunnelAddress,
-        ];
+        // TO DO: Route the prefix through the existing tunnel address
+
+        return $tunnelPrefix;
     }
+
 }
