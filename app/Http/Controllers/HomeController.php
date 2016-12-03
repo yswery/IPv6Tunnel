c6266f4f84fbbd3c13d8a01c3d1296e40c460c58
Yif Swery <yiftachswr@gmail.com>
2016-12-04 01:11:06 +1300

    Display the user's tunnels and routed prefixes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Tunnel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $user = Auth::user();
+
+        return view('home')->with('tunnels', $user->tunnels);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,22 @@
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    You are logged in!
+                    @foreach($tunnels as $tunnel)
+                        <h3>Tunnel Info</h3>
+                        Remove Tunnel Server IPv4 Address: <strong>{{ $tunnel->local_v4_address }}</strong><br />
+                        Local Tunnel Client IPv4 Address: <strong>{{ $tunnel->remote_v4_address }}</strong><br />
+                        <br />
+                        Server IPv6 Address: <strong>{{ $tunnel->local_tunnel_address }}</strong><br />
+                        Local IPv6 Address: <strong>{{ $tunnel->remote_tunnel_address }}</strong><br />
+                        <br />
+                        MTU: <strong>{{ $tunnel->mtu_size }}</strong><br />
+                        <h3>Routed Prefixes</h3>
+                        @foreach($tunnel->prefixes as $prefix)
+                            - <strong>{{ $prefix->address . '/' . $prefix->cidr }}</strong><br />
+                        @endforeach
+                        <br />
+                        <hr />
+                    @endforeach
                 </div>
             </div>
         </div>
