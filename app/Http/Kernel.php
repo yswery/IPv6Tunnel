4c4b3c54afd87c0949204cc963b32de2d2006d10
Yif Swery <yiftachswr@gmail.com>
2016-12-09 21:14:19 +1300

    Remove CSRF and API throttle

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -28,12 +28,10 @@ class Kernel extends HttpKernel
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
 
         'api' => [
-            'throttle:60,1',
             'bindings',
         ],
     ];
