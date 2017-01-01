7bdea01ef260735ff2af895b5537b229af7b1ea9
Yif Swery <yiftachswr@gmail.com>
2017-01-01 21:39:31 +1300

    Remove the limit of the width

--- resources/views/tunnels/list.blade.php
+++ resources/views/tunnels/list.blade.php
@@ -3,40 +3,41 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Your Tunnels</div>
-                    {!! dump($tunnels[0]) !!}
-                    <div class="panel-body">
-                        <div class="table">
-                            <table class="table table-bordered table-hover">
-                                <thead>
+            <div class="panel panel-default">
+                <div class="panel-heading">Your Tunnels</div>
+                <div class="panel-body">
+                    <div class="table">
+                        <table class="table table-bordered table-hover">
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Server Name</th>
+                                    <th>Server IP</th>
+                                    <th>Client IP</th>
+                                    <th>Client Tunnel IP</th>
+                                    <th>Server Tunnel IP</th>
+                                    <th>Modified</th>
+                                    <th>Edit</th>
+                                    <th>Delete</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($tunnels as $tunnel)
                                     <tr>
-                                        <th>ID</th>
-                                        <th>Server Name</th>
-                                        <th>Server IP</th>
-                                        <th>Client IP</th>
-                                        <th>Created Date</th>
-                                        <th>Server Name</th>
+                                        <td>{{ $tunnel->id }}</td>
+                                        <td>{{ $tunnel->server->name }}</td>
+                                        <td>{{ $tunnel->local_v4_address }}</td>
+                                        <td>{{ $tunnel->remote_v4_address }}</td>
+                                        <td>{{ $tunnel->local_tunnel_address }}</td>
+                                        <td>{{ $tunnel->remote_tunnel_address }}</td>
+                                        <td>{!! date('j\<\s\u\p\>S\<\/\s\u\p\> F Y', strtotime($tunnel->updated_at)) !!}</td>
+                                        <td class="center-td"><span class="glyphicon glyphicon-edit"></span></td>
+                                        <td class="center-td"><span class="glyphicon glyphicon-remove"></span></td>
                                     </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach($tunnels as $tunnel)
-                                        <tr>
-                                            <td>{{ $tunnel->id }}</td>
-                                            <td>{{ $tunnel->server }}</td>
-                                            <td>Foo</td>
-                                            <td>Foo</td>
-                                            <td>Foo</td>
-                                            <td>Foo</td>
-                                        </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
-                        </div>
+                                @endforeach
+                            </tbody>
+                        </table>
                     </div>
-
-
                 </div>
             </div>
         </div>
