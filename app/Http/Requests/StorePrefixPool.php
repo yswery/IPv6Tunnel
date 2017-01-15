c921d764e43999dc1a7456b2fa2a137e5c793a3c
Yif Swery <yiftachswr@gmail.com>
2017-01-16 00:43:51 +1300

    Create Prefix pool store validation

---
+++ app/Http/Requests/StorePrefixPool.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StorePrefixPool extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'tunnel_server_id' => 'required|integer|exists:tunnel_servers,id',
+            'address'          => 'required|string|ip|unique:prefix_pool,address',
+            'cidr'             => 'required|integer',
+        ];
+    }
+}
