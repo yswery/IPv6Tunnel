de6fef4842b23f7821b0f33bfc9049b57e0595ec
Yif Swery <yiftachswr@gmail.com>
2017-01-15 16:21:42 +1300

    Add modal to enter a new prefix into the pool

--- resources/views/admin/prefix-pool/index.blade.php
+++ resources/views/admin/prefix-pool/index.blade.php
@@ -9,7 +9,7 @@
                     Prefix Pool
                 </h3>
 
-                <button class="btn btn-success pull-right add-prefix-to-pool">Add Prefix Resource</button>
+                <button class="btn btn-success pull-right" data-toggle="modal" data-target="#addPrefixPoolModel">Add Prefix Resource</button>
                 <div class="clearfix"></div>
 
             </div>
@@ -47,6 +47,35 @@
                 </div>
             </div>
         </div>
+
+        <!-- Modal -->
+        <div class="modal fade" id="addPrefixPoolModel" tabindex="-1" role="dialog" aria-labelledby="addPrefixPoolModelLabel">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        <h4 class="modal-title" id="addPrefixPoolModelLabel">Add new Prefix to Pool</h4>
+                    </div>
+                    <div class="modal-body">
+                        <form>
+                            <div class="form-group">
+                                <label for="recipient-name" class="control-label">Recipient:</label>
+                                <input type="text" class="form-control" id="recipient-name">
+                            </div>
+                            <div class="form-group">
+                                <label for="message-text" class="control-label">Message:</label>
+                                <textarea class="form-control" id="message-text"></textarea>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
 </div>
 @endsection
