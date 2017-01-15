9c308c73543f86451b5fc31e75441f569460ef18
Yif Swery <yiftachswr@gmail.com>
2017-01-15 16:11:51 +1300

    Add base view for the list the of prefix pool

--- resources/views/admin/prefix-pool/index.blade.php
+++ resources/views/admin/prefix-pool/index.blade.php
@@ -4,35 +4,42 @@
 <div class="container">
     <div class="row">
         <div class="panel panel-default">
-            <div class="panel-heading">Your Tunnels</div>
+            <div class="panel-heading">
+                <h3 class="panel-title pull-left">
+                    Prefix Pool
+                </h3>
+
+                <button class="btn btn-success pull-right add-prefix-to-pool">Add Prefix Resource</button>
+                <div class="clearfix"></div>
+
+            </div>
+
             <div class="panel-body">
                 <div class="table">
                     <table class="table table-bordered table-hover">
                         <thead>
                         <tr>
                             <th>ID</th>
+                            <th>Prefix</th>
+                            <th>Server Location</th>
                             <th>Server Name</th>
-                            <th>Server IP</th>
-                            <th>Client IP</th>
-                            <th>Server Tunnel IP</th>
-                            <th>Client Tunnel IP</th>
+                            <th>Sub Prefixes</th>
                             <th>Modified</th>
                             <th>Edit</th>
                             <th>Delete</th>
                         </tr>
                         </thead>
                         <tbody>
-                        @foreach($tunnels as $tunnel)
+                        @foreach($prefixPool as $prefix)
                         <tr>
-                            <td>{{ $tunnel->id }}</td>
-                            <td>{{ $tunnel->server->name }}</td>
-                            <td>{{ $tunnel->local_v4_address }}</td>
-                            <td>{{ $tunnel->remote_v4_address }}</td>
-                            <td>{{ $tunnel->local_tunnel_address }}</td>
-                            <td>{{ $tunnel->remote_tunnel_address }}</td>
-                            <td>{!! date('j\<\s\u\p\>S\<\/\s\u\p\> F Y', strtotime($tunnel->updated_at)) !!}</td>
-                            <td class="center-td"><a href="{{ route('tunnels.details', ['tunnel_id' => $tunnel->id]) }}"><span class="glyphicon glyphicon-edit"></span></a></td>
-                            <td class="center-td"><a href="{{ route('tunnels.delete', ['tunnel_id' => $tunnel->id]) }}"><span class="glyphicon glyphicon-remove"></span></a></td>
+                            <td>{{ $prefix->id }}</td>
+                            <td>{{ $prefix->prefix }}</td>
+                            <td>{{ $prefix->server->country_code }}</td>
+                            <td>{{ $prefix->server->name }}</td>
+                            <td>{{ $prefix->subPrefixes()->count() }}</td>
+                            <td>{!! date('j\<\s\u\p\>S\<\/\s\u\p\> F Y', strtotime($prefix->updated_at)) !!}</td>
+                            <td class="center-td"><a href="#"><span class="glyphicon glyphicon-edit"></span></a></td>
+                            <td class="center-td"><a href="#"><span class="glyphicon glyphicon-remove"></span></a></td>
                         </tr>
                         @endforeach
                         </tbody>
