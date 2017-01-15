b5f07c2ff978906604e2a65071e7550fc21d9556
Yif Swery <yiftachswr@gmail.com>
2017-01-15 16:21:26 +1300

    Return the tunnel servers available to the prefix pool page

--- app/Http/Controllers/Admin/PrefixPoolController.php
+++ app/Http/Controllers/Admin/PrefixPoolController.php
@@ -2,16 +2,23 @@
 
 namespace App\Http\Controllers\Admin;
 
-
 use App\Http\Controllers\Controller;
 use App\Models\PrefixPool;
+use App\Models\TunnelServer;
 
 class PrefixPoolController extends Controller
 {
     public function index()
     {
-        $prefixPool = PrefixPool::with('server')->get();
+        $prefixPool    = PrefixPool::with('server')->get();
+        $tunnelServers = TunnelServer::all();
 
-        return view('admin.prefix-pool.index')->with('prefixPool', $prefixPool);
+        return view('admin.prefix-pool.index')
+            ->with('prefixPool', $prefixPool)
+            ->with('tunnelServers', $tunnelServers);
+    }
+
+    function new () {
+        // create the prefix here and return resposne in json
     }
 }
