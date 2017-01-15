e06c729a4c864a23ad9817047a16ff022c23d608
Yif Swery <yiftachswr@gmail.com>
2017-01-15 23:09:23 +1300

    Set the fillable and hidden on the tunnel server model

--- app/Models/TunnelServer.php
+++ app/Models/TunnelServer.php
@@ -8,6 +8,19 @@ class TunnelServer extends Model
 {
     protected $table = 'tunnel_servers';
 
+    protected $fillable = [
+        'address',
+        'name',
+        'country_code',
+        'city',
+        'ssh_password',
+        'ssh_port',
+    ];
+
+    protected $hidden = [
+        'ssh_password',
+    ];
+
     public function prefixes()
     {
         return $this->hasMany(TunnelPrefix::class);
