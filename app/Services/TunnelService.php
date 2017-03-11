d3703ab5840166e654dfd46d9202a215ecfc0295
Yif Swery <yiftachswr@gmail.com>
2017-03-12 10:56:18 +1300

    Use the new tunnel server method for the sshExec

--- app/Services/TunnelService.php
+++ app/Services/TunnelService.php
@@ -32,11 +32,11 @@ class TunnelService
         $tunnel->save();
 
         // Provision the tunnel address and interface on local tunnel server
-        \SSH::into($tunnelServer->name)->run([
+        $tunnelServer->sshExec([
             'ip tunnel add ' . $tunnel->local_interface . ' mode sit remote ' . $tunnel->remote_v4_address . ' local ' . $tunnel->local_v4_address . ' ttl 255',
             'ip link set ' . $tunnel->local_interface . ' up',
             'ip link set dev ' . $tunnel->local_interface . ' mtu ' . $tunnel->mtu_size,
-            'ip addr add ' . $tunnel->local_tunnel_address . '/64 dev ' . $tunnel->local_interface,
+            'ip addr add ' . $tunnel->local_tunnel_address . '/' . $tunnelIpv6Addresses['cidr'] . ' dev ' . $tunnel->local_interface,
         ]);
 
         $tunnelPrefix = $this->allocateTunnelPrefix($tunnelServer, $tunnel, $cidrSize);
@@ -59,7 +59,7 @@ class TunnelService
         $tunnelPrefix->save();
 
         // Route the prefix through the existing tunnel address
-        \SSH::into($tunnelServer->name)->run([
+        $tunnelServer->sshExec([
             'ip route add ' . $tunnelPrefix->address . '/' . $tunnelPrefix->cidr . ' dev ' . $tunnel->local_interface,
         ]);
 
@@ -78,7 +78,7 @@ class TunnelService
         }
 
         // Provision the tunnel address and interface on local tunnel server
-        \SSH::into($tunnel->server->name)->run([
+        $tunnel->server->sshExec([
             'ip addr del ' . $tunnel->local_tunnel_address . '/64 dev ' . $tunnel->local_interface,
             'ip link set ' . $tunnel->local_interface . ' down',
             'ip tunnel del ' . $tunnel->local_interface,
@@ -98,7 +98,7 @@ class TunnelService
     public function removeTunnelPrefix(TunnelPrefix $tunnelPrefix)
     {
         // Remove the static route from the tunnel server node
-        \SSH::into($tunnelPrefix->server->name)->run(
+        $tunnelPrefix->server->sshExec(
             'ip route del ' . $tunnelPrefix->address . '/' . $tunnelPrefix->cidr . ' dev ' . $tunnelPrefix->tunnel->local_interface
         );
 
